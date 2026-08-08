c4eb196350149957e8040187704f29a181310700
Kamal Alshinawi <kamalalshinawi@gmail.com>
2026-08-08 08:05:31 +0300

    feat: 10_Switch

---
+++ JavaScript Fundamentals/10_Switch/switch_expressions.js
@@ -0,0 +1,17 @@
+let age = 25;
+
+switch (true) {
+    case age < 13:
+        console.log("Child");
+        break;
+    case age < 20:
+        console.log("Teenager");
+        break;
+    case age < 30:
+        console.log("Young Adult");
+        break;
+    default:
+        console.log("Adult");
+}
+// Outputs: Young Adult
+
