5808e5a19aaab3d70f531a4c457ef2dd71168b79
Kamal Alshinawi <kamalalshinawi@gmail.com>
2026-08-08 08:25:49 +0300

    feat: loops and functions

---
+++ JavaScript Fundamentals/11_Loops/for_of_loop.js
@@ -0,0 +1,19 @@
+let name = "JavaScript";
+
+for (let char of name) {
+    console.log("Character: " + char);
+}
+
+
+
+// Using break
+for (let i = 0; i < 10; i++) {
+    if (i === 5) break;
+    console.log(i);
+}
+
+// Using continue
+for (let i = 0; i < 5; i++) {
+    if (i === 2) continue;
+    console.log(i);
+}
