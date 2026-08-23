b0ff3192e2ebfdf406af3530fa43e0eed6e6f851
Kamal Alshinawi <kamalalshinawi@gmail.com>
2026-08-23 08:20:51 +0300

    feat: remove and removeChild

--- Understanding the DOM & DOM Manipulation/18_DOM_Create_Remove/script.js
+++ Understanding the DOM & DOM Manipulation/18_DOM_Create_Remove/script.js
@@ -1,44 +1,41 @@
-let newParagraph = document.createElement('p');
+let newParagraph = document.createElement("p");
 
-newParagraph.textContent = 'this is a new strings';
+newParagraph.textContent = "this is a new strings";
 
-newParagraph.className = 'newPara' ;
+newParagraph.className = "newPara";
 
-let container1 = document.getElementById('container1');
+let container1 = document.getElementById("container1");
 
-container1.appendChild(newParagraph)
+container1.appendChild(newParagraph);
 
-
-let newButton = document.createElement('button')
+let newButton = document.createElement("button");
 
 newButton.textContent = "Click me!";
 
+let container2 = document.getElementById("container2");
 
-let container2 = document.getElementById('container2');
-
-container2.appendChild(newButton)
-
+container2.appendChild(newButton);
 
 // Create an image
-let newImage = document.createElement('img');
-newImage.src = 'https://placehold.co/150';
-newImage.alt = 'Placeholder image';
+let newImage = document.createElement("img");
+newImage.src = "https://placehold.co/150";
+newImage.alt = "Placeholder image";
 container1.appendChild(newImage);
 
+let heading1 = document.createElement("h1");
 
-
-let heading1 = document.createElement('h1');
-
-heading1.textContent = "this is alshinawi"; 
+heading1.textContent = "this is alshinawi";
 
 let specificH2 = document.getElementsByTagName("h2")[0];
 
+container1.insertBefore(heading1, specificH2);
 
-container1.insertBefore(heading1,specificH2)
+let button = container2.querySelector("button");
 
-
-let button = container2.querySelector('button')
-
-if(button){
-    container1.appendChild(button)
+if (button) {
+  container1.appendChild(button);
 }
+
+button.remove();
+let img = document.querySelector("img");
+container1.removeChild(img);
