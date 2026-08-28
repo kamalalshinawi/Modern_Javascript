fd212b81f2cb1724ef4ddf7ba32609f5313003a7
Kamal Alshinawi <kamalalshinawi@gmail.com>
2026-08-28 19:02:24 +0300

    feat: stopPropagation and preventDefault

--- JavaScript Events/20_Events_Practice/script.js
+++ JavaScript Events/20_Events_Practice/script.js
@@ -1,41 +1,25 @@
-const nameInput = document.getElementById('nameInput');
+let div1 = document.getElementById("div1");
+let button = document.getElementById("button");
+let link = document.getElementById("link");
 
-nameInput.addEventListener('keydown', (event) => {
-    // Let's look at what's inside the event object
-    console.log('Event object:', event);
-
-    // Most commonly used properties:
-    console.log('Key pressed:', event.key);
-    console.log('Target element:', event.target);
-    console.log('Type of event:', event.type);
-
-    // Getting input value
-    console.log('Current input value:', event.target.value);
+// when clicked on div1 will print div clicked only
+div1.addEventListener("click", () => {
+  console.log("div clicked");
 });
 
+// when clicked on button will print button clicked and div also
+// button.addEventListener('click',(event)=>{
 
-nameInput.addEventListener('input', (event) => {
-    // These do the same thing:
-    const value1 = event.target.value;
-    const value2 = nameInput.value;
+//     console.log('button clicked')
+// })
 
-    console.log('User typed:', value1);
+// use StopPropagation and will use this event only and never mind the parent events
+button.addEventListener("click", (event) => {
+  event.stopPropagation();
+  console.log("button clicked");
 });
 
-// customMenu
-
-const customMenu = document.getElementById('customMenu');
-
-document.addEventListener('contextmenu', (event) => {
-    event.preventDefault(); // Prevent default context menu
-
-    // Position and show our custom menu
-    customMenu.style.display = 'block';
-    customMenu.style.left = event.clientX + 'px';
-    customMenu.style.top = event.clientY + 'px';
-});
-
-// Hide menu when clicking elsewhere
-document.addEventListener('click', () => {
-    customMenu.style.display = 'none';
+// now when clicked on the link wi'll not navigate to google
+link.addEventListener("click", (event) => {
+  event.preventDefault();
 });
