faa538b30eabf797c2380f217a7e2f9fa5e012dd
Kamal Alshinawi <kamalalshinawi@gmail.com>
2026-08-30 15:14:44 +0300

    feat: simple example of what's the async code

---
+++ Async JavaScript/22_Async_Introduction/async.js
@@ -0,0 +1,23 @@
+let testAsync = () => {
+  let timeNow = Date.now();
+  while (Date.now() - timeNow < 5000) {}
+  return `will print this after loop 5 second `;
+};
+
+console.log("start the program");
+testAsync(); // will freeze here for 5 second
+console.log(`end the program`);
+
+//  this is how setTimeout work in Async
+
+console.log("start program");
+
+let asyncFun = (data) => {
+  console.log("the start of the function");
+
+  setTimeout(() => {
+    console.log(`this is Async code ? ${data} `);
+  }, 2000);
+};
+asyncFun(`true`);
+console.log(`this Ending of Program`);
