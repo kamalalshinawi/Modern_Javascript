47d908c6e66d81055d3b2af74792bd35cc63a7cd
Kamal Alshinawi <kamalalshinawi@gmail.com>
2026-09-02 14:26:01 +0300

    feat: share property in the methods

--- Object-Oriented JavaScript/28_Constructors_And_Prototypes/constructors.js
+++ Object-Oriented JavaScript/28_Constructors_And_Prototypes/constructors.js
@@ -22,3 +22,30 @@ console.log(user1.greet === user2.greet); // true - memory efficient!
 user1.hobby = "reading";
 console.log(user1.hobby); // "reading"
 console.log(user2.hobby); // undefined
+
+
+
+function Car(make, model) {
+    // Instance properties (unique to each car)
+    this.make = make;
+    this.model = model;
+    this.isRunning = false;
+}
+
+// Prototype methods (shared by all cars)
+Car.prototype.start = function() {
+    this.isRunning = true;
+    console.log('Vroom!');
+};
+
+Car.prototype.stop = function() {
+    this.isRunning = false;
+    console.log('Stopping...');
+};
+
+const car1 = new Car('Toyota', 'Camry');
+const car2 = new Car('Honda', 'Civic');
+
+car1.start();
+console.log(car1.isRunning); // true
+console.log(car2.isRunning); // false
